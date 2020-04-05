10a907c30b6f13b15f5e99cdf8f738708b421d01
Mwp98 <mwp1998@tamu.edu>
2020-04-05 00:11:54 -0500

    Update Addnums.py
    Adding a number

--- Python/Addnums.py
+++ Python/Addnums.py
@@ -1,8 +1,9 @@
 # Program to add two number
 num1=5
 num2=20
+num3=4
 # adding the numbers
-sum = num1 + num2
+sum = num1 + num2-num3
 
 # print value out
- print("The Sum of the two numbers is", sum)
+ print("The Sum of the two numbers minus a third number is", sum)
